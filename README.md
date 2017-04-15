0f8f729fe828b4943f1d2f494d2b871041274f05
Simon Greene <simongreene02@gmail.com>
2017-04-15 10:57:54 -0500

    Made EmployeeDAO testable and added Readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Instructions
+## How to build the web application
+- Change the directory to the scripts folder inside the project.
+    ```sh
+    cd ~/workspace/Website/scripts/
+    ```
+- Run the build script like so:
+    ```sh
+    ./build.sh
+    ```
+- Run the deploy script like so:
+    ```sh
+    ./deploy.sh
+    ```
+- Go to the newly created CATALINA_BASE directory, which by default will be called "newsimon-web"
+    ```sh
+    cd ~/newsimon-web/
+    ```
+- Run the restart script like so:
+    ```sh
+    ./restart.sh
+    ```
+- Open a web browser and go to the url `http://localhost:8080`
+
+
+
+
+Made using http://dillinger.io/
--- src/main/java/org/greatworksinc/newsimon/EmployeeDAO.java
+++ src/main/java/org/greatworksinc/newsimon/EmployeeDAO.java
@@ -18,27 +18,33 @@ public class EmployeeDAO {
   private static final Logger log = LoggerFactory.getLogger(EmployeeDAO.class);
   // JDBC driver name and database URL
   private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-  //private static final String DB_URL = "jdbc:mysql://localhost/workers&useSSL=false";
+  // private static final String DB_URL =
+  // "jdbc:mysql://localhost/workers&useSSL=false";
   private static final String DB_URL = "jdbc:mysql://localhost/workers?verifyServerCertificate=false&serverTimezone=UTC&useSSL=true";
   // Database credentials
   private static final String USER = "root";
   private static final String PASS = "simongreene";
   private static final String ALL_EMPLOYEES = "select * from Employee";
   private static final String EMPLOYEES_BY_DEPARTMENT = "select * from Employee where Department = '";
+  private final Connection conn;
 
   public EmployeeDAO() {
     try {
       Class.forName(JDBC_DRIVER);
-    } catch (ClassNotFoundException e) {
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+    } catch (ClassNotFoundException | SQLException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public EmployeeDAO(Connection conn) {
+    this.conn = conn;
   }
 
   public List<Employee> findEmployees() {
     ImmutableList.Builder<Employee> employees = ImmutableList.builder();
 
-    try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-        Statement stmt = conn.createStatement();
+    try (Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(ALL_EMPLOYEES)) {
       while (rs.next()) {
         Employee employee = Employee.builder().name(rs.getString("Name")).job(rs.getString("Title"))
@@ -57,7 +63,7 @@ public class EmployeeDAO {
 
     try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
         Statement stmt = conn.createStatement();
-        //TODO validate department to avoid XSS 
+        // TODO validate department to avoid XSS
         ResultSet rs = stmt.executeQuery(EMPLOYEES_BY_DEPARTMENT + department + "'")) {
       while (rs.next()) {
         Employee employee = Employee.builder().name(rs.getString("Name")).job(rs.getString("Title"))
